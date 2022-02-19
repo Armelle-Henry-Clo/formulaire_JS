6fec36b6a96118b886dc1ddc4f20143249f408fe
Armelle HENRY_Clo81 <clotildearmelle@gmail.com>
2022-02-19 01:25:46 +0100

    ajout border

--- Js/app.js
+++ Js/app.js
@@ -1,38 +1,82 @@
 
- let btnValid = document.getElementById('btn');
- let prenom = document.getElementById('name');
- let mail = document.getElementById('email');;
- let mdp1 = document.getElementById('mdp');
- let mdp2 = document.getElementById('mdp');
+//----input Nom--------
 
-// btnValid.addEventListener('click', inscription());
+let nom = document.getElementById('name');
+    nom.style.borderColor = "blue";
+    
+function monNom(){
+
+    if(nom !== null){
+        nom.style.borderColor = "green";
+        
+    }else{
+        nom.style.borderColor = "red";
+        alert("Vous devez mettre votre nom!")
+    };
+};
+
+
+///------------input Email
+
+let mail = document.getElementById('email');
+    mail.style.borderColor = "blue";
+
+function monMail(){
+
+    if (mail.value.match(/[a-z0-9_\-\.]+@[a-z0-9_\-\.]+\.[a-z]+/i))
+    {
+        mail.style.borderColor = "green";
+
+    }else{
+        mail.style.borderColor = "red";
+        alert("votre mail n'est pas valide!");
+    };  
+};
+
+
+//------Confirmation mot de passe---------
+
+let mdp1 = document.getElementById('mdp1');
+let mdp2 = document.getElementById('mdp2');
+
+     mdp1.style.borderColor = "blue";
+     mdp2.style.borderColor = "blue";
+
+  function monMdp(){ 
+    
+    let z = mdp1.value;
+        a = mdp2.value; 
+
+    if(z === a ){
+        mdp1.style.borderColor = "green";
+        mdp2.style.borderColor = "green";
+
+    }else{
+        mdp1.style.borderColor = "red";
+        mdp2.style.borderColor = "red";
+        alert("Veuillez taper le même mot de passe");
+    };
+
+  };
+
+
+//-----------evenement----
+
+let btnValid = document.getElementById('btn');     
+    // btnValid.addEventListener('click',inscription());
 
 function inscription(){
-    if(mdp1.textContent === mdp2.textContent){
-        console.log('yes!');
+
+    if(mail.style.borderColor == 'green'&& nom.style.borderColor == 'green' && mdp2.style.borderColor == 'green'){
+        
+    let titre = document.getElementById('titre');
+
+    titre.style.color = "green";
+
+    titre.innerHTML = `Bravo ${nom.value}! Un mail de confirmation vient de vous être envoyé sur ${mail.value}.`;
     }else{
-        console.log('No');
-    }
-  };
 
-  function identite(){
-    if(prenom.textContent === ''){
-        console.log('prénom!');
-    }else{
-        console.log('No prenom');
-    }
-  };
+        alert(`Vous devez remplir tous les champs pour vous inscrire!`);
+    }        
 
-  function mailing(){
-    if(mail.textContent == ){
-        console.log('votre mail!');
-    }else{
-        console.log('pas de mail');
-    }
-  };
-
-identite();
-mailing();
-inscription();
-
-
+};
